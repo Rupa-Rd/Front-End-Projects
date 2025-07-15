42c1e62d6cb88b97a484f9d9c3d33a05dc111d2f
Rupa-Rd <roopa.69.spr@gmail.com>
2025-07-16 00:18:34 +0530

    Birthday remainder app is completed

--- birthday-remainder/app/src/App.js
+++ birthday-remainder/app/src/App.js
@@ -1,11 +1,27 @@
-import './App.css';
-
+import React, { useState } from 'react'
+import data from './data/data'
+import List from './List'
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    // create a state variable here
+    const [people, setPeople] = useState(data)
+
+    // this should clear all records
+    function clearAllRecords() {
+        setPeople([])
+    }
+    return (
+        <main>
+          <h1>Birthday Reminder</h1>
+            <section className="container">
+                <h3>{people.length} birthdays today</h3>
+                <List people={people} />
+                <button onClick={clearAllRecords}>Clear All</button>
+            </section>
+            <a href="https://www.guvi.in/" className="copywright">
+        Made by Guvi💚
+      </a>
+        </main>
+    )
 }
 
-export default App;
+export default App
